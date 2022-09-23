765db3b56182e5b541ea0b37006624d9869638f0
gmirg <gofthet@gmail.com>
2022-09-24 01:42:56 +0700

    ejercicio1.1 resuelto

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,30 +1,26 @@
 function par_25() {
     let num1 = parseInt(prompt('Primer número:'));
-    let num2 = parseInt(prompt('Primer número:'));
-    debugger;
-    if ((num1 % 2 == 0 && num1 % 25 == 0) && (num2 % 2 == 0 && num2 % 25 == 0)) {
-        alert("Los dos números son pares y múltiplos de 25")
-    } else {
-        do {
-            debugger;
-            if ((num1 % 2 == 0 && num1 % 25 == 0) && !(num2 % 2 == 0 && num2 % 25 == 0)) { // num1 es par25 pero num2 no lo es
-                alert("Solo el primer número es par y múltiplo de 25");
-            } else { // num2 es par25 pero num1 no lo es
-                alert("Solo el segundo número es par y múltiplo de 25");
-            }
-        } while ((num1 % 2 != 0 && num1 % 25 != 0) || (num2 % 2 != 0 && num2 % 25 != 0))
+    let num2 = parseInt(prompt('Segundo número:'));
+    switch (true) {
+        case ((num1 % 2 == 0) && (num1 % 25 == 0)) && ((num2 % 2 == 0) && (num2 % 25 == 0)):
+            alert("Los dos números son pares y múltiplos de 25")
+            break;
+        case ((num1 % 2 == 0) && (num1 % 25 == 0)) && !((num2 % 2 == 0) && (num2 % 25 == 0)):
+            alert("Solo el primer número es par y múltiplo de 25");
+            break;
+        case !((num1 % 2 == 0) && (num1 % 25 == 0)) && ((num2 % 2 == 0) && (num2 % 25 == 0)):
+            alert("Solo el segundo número es par y múltiplo de 25");
+            break;
+        default: alert("Ninguno de los números es par y múltiplo de 25");
+            break;
     }
 }
-function emp_term() {
-var palabra = prompt('Inserta una palabra').toLocaleLowerCase();
-var primeraLetra = (palabra.split("")); 
-debugger; 
-} 
 
-// var cont = 0;
-// for(var i = 0; i<palabra.length;i++){
-//   if(letra=="frase.[0]){
-//     cont++;
-//   }
+// function emp_term() {
+// var palabra = prompt('Inserta una palabra').toLocaleLowerCase();
+// var arr = (palabra.split("")); 
+// var primeraLetra = arr
 // }
-// alert('La letra '+ letra + 'aparece '+ cont +' veces');
+// alert();
+// // Mi intención era extraer la primera y última letra de la palabra y mediante condicionales simples dar el resultado si empieza y termina por A,
+// //si empieza por A, si termina por A, o si no empieza ni termina por A. 
---
+++ ejercicio2/script.js
@@ -0,0 +1,25 @@
+    //  Entradas: frase
+    // Solucion:
+    // Solicitamos la frase y creamos un contador igual a cero, mediante un for que empieza de cero, extraemos la cantidad de espacios que encontremos en la longitud de la cadena
+    // 
+    // Salida: Cuantas palabras tiene esta frase (nos la da el contador)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
