d346dec2c995dd372173573de1aee1743d56972c
gmirg <gofthet@gmail.com>
2022-09-24 13:16:50 +0700

    Ejercicio 2 completo

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -1,25 +1,8 @@
-    //  Entradas: frase
-    // Solucion:
-    // Solicitamos la frase y creamos un contador igual a cero, mediante un for que empieza de cero, extraemos la cantidad de espacios que encontremos en la longitud de la cadena
-    // 
-    // Salida: Cuantas palabras tiene esta frase (nos la da el contador)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let frase = prompt("Inserta cualquier frase");
+let counter = 0;
+for (let i = 0; i < frase.length; i++) {
+        if (frase.charAt(i) === " ") {
+            counter++;
+    }
+}
+alert("Tu frase tiene " + (counter + 1) + " palabras"); 
